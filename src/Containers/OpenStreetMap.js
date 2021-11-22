08a6419662dbf8060943b99731f4f0630350a967
Suvrat <suvrat72000@gmail.com>
2021-11-22 12:10:11 +0530

    Update commit

--- src/Containers/OpenStreetMap.js
+++ src/Containers/OpenStreetMap.js
@@ -1,7 +1,14 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import { MapsComponent, LayersDirective, NavigationLineDirective, LayerDirective, Zoom, MarkersDirective, NavigationLine, NavigationLinesDirective, MarkerDirective, Marker, Inject } from '@syncfusion/ej2-react-maps';
 
 export default function OpenStreetMap(){
+    navigator.geolocation.getCurrentPosition(function(position) {
+        const latitude = position.coords.latitude
+        const longitude = position.coords.longitude
+        console.log("Latitude is :", latitude);
+        console.log("Longitude is :",longitude );
+      });
     function MapPlaceholder() {
         return (
           <p>
@@ -11,17 +18,33 @@ export default function OpenStreetMap(){
         )
       }
     // const position = [51.505, -0.09]
+    // 28.7041° N, 77.1025° E
     return(
-        <MapContainer
-      center={[51.505, -0.09]}
-      zoom={1}
-        style={{ height: '100vh', width: '100wh' }}
-      scrollWheelZoom={false}
-      placeholder={<MapPlaceholder />}>
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-    </MapContainer>
+        <MapsComponent id="maps" zoomSettings={{ enable: true, toolbars: ['Zoom', 'ZoomIn', 'ZoomOut', 'Pan', 'Reset'] }}>
+            {/* <Inject services={[Zoom]}/> */}
+            <Inject services={[Marker, NavigationLine, Zoom]}/>
+                <LayersDirective>
+                    <LayerDirective layerType='OSM'>
+                        <MarkersDirective>
+                            <MarkerDirective visible={true} height={25} width={15} dataSource={[
+    {
+        latitude: 28.603112,
+        longitude: 77.366885,
+        name: "California"
+    },
+    {
+        latitude: 28.7041,
+        longitude: 77.1025,
+        name: "New York"
+    }
+]}>
+                            </MarkerDirective>
+                        </MarkersDirective>
+                        <NavigationLinesDirective>
+                            <NavigationLineDirective visible={true} latitude={[28.603112, 28.7041]} longitude={[77.366885, 77.1025]} color="blue"  width={5}/>
+                        </NavigationLinesDirective>
+                    </LayerDirective>
+                </LayersDirective>
+            </MapsComponent>
     );
 }
